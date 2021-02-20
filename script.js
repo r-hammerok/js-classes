e9f977b112a7874235c55707c8d6a12acb59684e
Roman Ivannikov <roman.y.ivannikov@gmail.com>
2021-02-20 16:56:50 +0700

    build: lesson 15 normal part two

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+class First {
+    hello() {
+        console.log('Привет! Я метод родителя!');
+    }
+}
+
+class Second extends First {
+    hello() {
+        super.hello();
+        console.log('А я наследуемый метод!');
+    }
+}
+
+const first = new First();
+const second = new Second();
+
+first.hello();
+second.hello();
